e67933f8d2922be3442ebc4b2bfd05ad5b1324c3
ahmadnadeem123 <ahmadnadeempucit@gmail.com>
2021-11-07 14:43:37 +0500

    Secound commit

--- Task-Three/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Task-Three/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    b1=findViewById(R.id.button);
+    b1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openWebPage("https://github.com/ahmadnadeem123/Quiz-App");
+        }
+    });
+
     }
+
+
+
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
 }
