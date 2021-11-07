991dd27c3c04b292d295fa1d4747a5e50b4bdd10
ahmadnadeem123 <ahmadnadeempucit@gmail.com>
2021-11-07 22:43:53 +0500

    Fifth commit

--- Task-Three/app/src/main/java/com/example/myapplication/Practice.java
+++ Task-Three/app/src/main/java/com/example/myapplication/Practice.java
@@ -2,13 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Practice extends AppCompatActivity {
 
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+
+        b1=findViewById(R.id.b1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent obj=new Intent(Practice.this, Practice1.class);
+                startActivity(obj);
+            }
+        });
     }
 }
