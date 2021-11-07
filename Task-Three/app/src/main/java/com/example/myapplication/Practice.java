fd56948bb6e9f60cf6f7212ae7f69975961228e3
ahmadnadeem123 <ahmadnadeempucit@gmail.com>
2021-11-08 00:32:45 +0500

    seventh commit

--- Task-Three/app/src/main/java/com/example/myapplication/Practice.java
+++ Task-Three/app/src/main/java/com/example/myapplication/Practice.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class Practice extends AppCompatActivity {
 
-    Button b1,b2;
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +17,32 @@ public class Practice extends AppCompatActivity {
 
         b1=findViewById(R.id.b1);
         b2=findViewById(R.id.b2);
+        b3=findViewById(R.id.b3);
+        b4=findViewById(R.id.b4);
+        b5=findViewById(R.id.b5);
+        b6=findViewById(R.id.b6);
+        b7=findViewById(R.id.b7);
+        b8=findViewById(R.id.b8);
+        b9=findViewById(R.id.b9);
+        b10=findViewById(R.id.b10);
+        b11=findViewById(R.id.button6);
+        b12=findViewById(R.id.button7);
 
+        b11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent obj=new Intent(Practice.this, MainActivity.class);
+                startActivity(obj);
+            }
+        });
+
+        b12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                moveTaskToBack(true);
+            }
+        });
 
 
 
@@ -32,6 +57,70 @@ public class Practice extends AppCompatActivity {
                 obj.putExtra("value", "Halqiyah");
                 obj.putExtra("value1","p1");
                 startActivity(obj);
+                break;
+
+            case R.id.b2:
+                Intent obj1=new Intent(Practice.this, Practice1.class);
+                obj1.putExtra("value", "Lahatiyah");
+                obj1.putExtra("value1","p2");
+                startActivity(obj1);
+                break;
+
+            case R.id.b3:
+                Intent obj2=new Intent(Practice.this, Practice1.class);
+                obj2.putExtra("value", "Shajariyah-Haafiyah");
+                obj2.putExtra("value1","p3");
+                startActivity(obj2);
+                break;
+
+            case R.id.b4:
+                Intent obj3=new Intent(Practice.this, Practice1.class);
+                obj3.putExtra("value", "Tarfiyah");
+                obj3.putExtra("value1","p4");
+                startActivity(obj3);
+                break;
+
+            case R.id.b5:
+                Intent obj4=new Intent(Practice.this, Practice1.class);
+                obj4.putExtra("value", "Nit-eeyah");
+                obj4.putExtra("value1","p5");
+                startActivity(obj4);
+                break;
+
+            case R.id.b6:
+                Intent obj5=new Intent(Practice.this, Practice1.class);
+                obj5.putExtra("value", "Lisaveyah");
+                obj5.putExtra("value1","p6");
+                startActivity(obj5);
+                break;
+
+            case R.id.b7:
+                Intent obj6=new Intent(Practice.this, Practice1.class);
+                obj6.putExtra("value", "MAKHARIJ 13");
+                obj6.putExtra("value1","p7");
+                startActivity(obj6);
+                break;
+
+            case R.id.b8:
+                Intent obj7=new Intent(Practice.this, Practice1.class);
+                obj7.putExtra("value", "Ghunna");
+                obj7.putExtra("value1","p8");
+                startActivity(obj7);
+                break;
+
+            case R.id.b9:
+                Intent obj8=new Intent(Practice.this, Practice1.class);
+                obj8.putExtra("value", "MAKHARIJ 15,16");
+                obj8.putExtra("value1","p9");
+                startActivity(obj8);
+                break;
+
+            case R.id.b10:
+                Intent obj9=new Intent(Practice.this, Practice1.class);
+                obj9.putExtra("value", "MAKHARIJ 17");
+                obj9.putExtra("value1","p10");
+                startActivity(obj9);
+                break;
 
         }
     }
--- Task-Three/app/src/main/java/com/example/myapplication/Practice1.java
+++ Task-Three/app/src/main/java/com/example/myapplication/Practice1.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +14,7 @@ public class Practice1 extends AppCompatActivity {
 
     TextView t1;
     ImageView v1;
+    Button b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,51 +28,73 @@ public class Practice1 extends AppCompatActivity {
         t1=findViewById(R.id.t1);
         t1.setText(obj.getStringExtra("value").toString());
         v1=findViewById(R.id.v1);
+        b1=findViewById(R.id.button3);
+        b2=findViewById(R.id.button4);
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                moveTaskToBack(true);
+            }
+        });
+
 
         String x=obj.getStringExtra("value1").toString();
-        if(x=="p1")
+
+        if(x.equals( "p1"))
         {
             v1.setImageResource(R.drawable.p1);
 
         }
 
-        else if(x=="p2")
+        else if(x.equals("p2"))
         {
             v1.setImageResource(R.drawable.p2);
         }
 
-        else if(x=="p3")
+        else if(x.equals("p3"))
         {
             v1.setImageResource(R.drawable.p3);
         }
-        else if(x=="p4")
+        else if(x.equals("p4"))
         {
             v1.setImageResource(R.drawable.p4);
         }
-        else if(x=="p5")
+        else if(x.equals("p5"))
         {
             v1.setImageResource(R.drawable.p5);
         }
-        else if(x=="p6")
+        else if(x.equals("p6"))
         {
             v1.setImageResource(R.drawable.p6);
         }
-        else if(x=="p7")
+        else if(x.equals("p7"))
         {
             v1.setImageResource(R.drawable.p7);
         }
-        else if(x=="p8")
+        else if(x.equals("p8"))
         {
             v1.setImageResource(R.drawable.p8);
         }
-        else if(x=="p9")
+        else if(x.equals("p9"))
         {
             v1.setImageResource(R.drawable.p9);
         }
-        else if(x=="p10")
+        else if(x.equals("p10"))
         {
             v1.setImageResource(R.drawable.p10);
         }
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent obj=new Intent(Practice1.this, Practice.class);
+                startActivity(obj);
+            }
+        });
 
 
 
